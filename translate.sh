2641784a09c2796b3c945b63d94432c38c2dc02a
Alex Grimes <alexkgrimes@gmail.com>
2019-06-12 15:44:33 -0500

    Clean up

--- translate.sh
+++ translate.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 #append the specifications after the parser
-python ir2smv.py -i FSM_V2.xml -o fsm.smv
+python ir2smv.py -i FSM.xml -o fsm.smv
 
 cat properties.txt >> fsm.smv
-
-dot -Tps UE.dot -o UE.ps
-dot -Tps NB.dot -o NB.ps
